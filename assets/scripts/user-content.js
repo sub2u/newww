021e5612f602dfd8b350d19fdd3912dd79868500
Zeke Sikelianos <zeke@sikelianos.com>
2015-01-27 14:14:14 -0800

    use a replaceState hack to hide anchor prefix from users

--- assets/scripts/user-content.js
+++ assets/scripts/user-content.js
@@ -1,28 +1,43 @@
 // for security reasons, generated-ids are now prefixed
 // with user-content- in READMEs, this snippet
-// checks whther someone has clicked on an
+// checks whether someone has clicked on an
 // auto-generated id.
 var $ = require("jquery")
 
 module.exports = function() {
-  $(user_content)
+  $(handleAnchorLinks)
 }
 
-var user_content = function() {
-  $(".content").on('click', 'a', function() {
+var handleAnchorLinks = function() {
+
+  // Go to anchor on page loading
+  if (window.location.hash.length) {
+    goToAnchor(window.location.hash.replace("#", ""))
+  }
+
+  // Go to anchor on readme hyperlink click
+  $(document).on('click', '.content a', function() {
     var match = $(this).attr('href').match(/^#(.*)$/)
-    var prefix = 'user-content-'
 
-    if (!match) return true // link was not anchor.
+    // link was not anchor
+    if (!match) return true
 
-    var anchor = $('#' + prefix + match[1] + ' a')
-
-    // we found a corresponding anchor #user-content-foo.
-    if (anchor.length) {
-      window.location.hash = prefix + match[1]
-      return false
-    }
-
-    return true
+    return goToAnchor(match[1])
   })
+}
+
+var goToAnchor = function(anchor) {
+
+  console.log("anchor", anchor)
+
+  var prefix = 'user-content-'
+  var el = $('#' + prefix + anchor + ' a')
+
+  if (!el.length) return true
+
+  var simpleAnchorUrl = window.location.href
+  window.location.hash = prefix + anchor
+  history.replaceState(null, null, simpleAnchorUrl)
+
+  return false
 }
